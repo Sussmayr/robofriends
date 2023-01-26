4c513b9dcffe9da12a363ecc1e587c059232f1a4
Sussmayr <51182004+Sussmayr@users.noreply.github.com>
2023-01-26 15:27:12 -0600

    Hello World first test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello'
+import 'tachyons';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Hello />
+  </React.StrictMode>
+);
+
+/*
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hello World</h1>
     <App />
   </React.StrictMode>
 );
+*/
+
+
+//ReactDOM.render(<Hello />, document.getElementByid('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
